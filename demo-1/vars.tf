7c37c9441591f078c7dd578071fb87a7fde7d261
rishikaushal2009 <rishikaushal2009@gmail.com>
2020-11-27 20:12:09 +0530

    addition of tfvars file

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,19 +1,17 @@
-variable "AWS_ACCESS_KEY" {
-}
+variable "AWS_ACCESS_KEY" {}
 
-variable "AWS_SECRET_KEY" {
-}
+variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-south-1"
 }
 
 variable "AMIS" {
-  type = map(string)
+  type = "map"
+
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1  = "ami-022758574f5a26580"
+    us-west-2  = "ami-02c8896b265d8c480"
+    ap-south-1 = "ami-05f3dcbda47f5afd1"
   }
 }
-
